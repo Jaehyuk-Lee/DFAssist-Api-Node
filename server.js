3da43162e20a7096c210edf4ebb05d3dda319201
Jaehyuk-Lee <alex00728@outlook.com>
2019-05-18 18:32:37 +0900

    add discord

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const config = require('./config');
 
-var Twit = require('twit');
-var express = require('express');
-var app = express();
+const Twit = require('twit');
+const express = require('express');
+const app = express();
+const request = require('request');
+const webhook = require("webhook-discord");
+
+const Hook = new webhook.Webhook(config.discordWebHookUrl);
 
 const PORT = process.env.PORT || 3000;
 
-var T = new Twit({
-  consumer_key:         'jo2B51c4tclG7y3QPSJCmdIlT',
-  consumer_secret:      'rX8e8mNnDl2T1LqbJJ25ki1LPPrSKal57FNWGJ3UKDENcKzUQB',
-  access_token:         '1106925839290032128-N5AM43Q88vp8KJzukhLzD01zCPXCEv',
-  access_token_secret:  '5ClHjc8VCeQn9W2qnw3yEEWbihpM35JuHMmBhkPT7amyN',
+const T = new Twit({
+  consumer_key:         config.twitter.consumer_key,
+  consumer_secret:      config.twitter.consumer_secret,
+  access_token:         config.twitter.access_token,
+  access_token_secret:  config.twitter.access_token_secret,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 });
@@ -22,6 +26,7 @@ app.get("/", function(req, res, next){
   'Content-Type, Authorization, Content-Length, X-Requested-With');
 
 
+  let service = req.query.service;
   let type = req.query.type;
   let name = req.query.name;
   let user = req.query.user;
@@ -33,15 +38,29 @@ app.get("/", function(req, res, next){
     return;
   }
 
-  let message = config.localization[type][lang].replace("{0}", name);
-
-  T.post('statuses/update', { status: "@" + user + " " + message }, function(err, data, response) {
-    console.log(data)
-    if(data.created_at != "")
-      res.send("0");
-    else if(data.errors.length != 0)
+  let message = " " + config.localization[type][lang].replace("{0}", name);
+  console.log(message);
+  if(service == "twitter"){
+    T.post('statuses/update', { status: "@" + user + message }, function(err, data, response) {
+      console.log(data);
+      if(data.created_at != "")
+        res.send("0");
+      else if(data.errors.length != 0)
+        res.send("1");
+    });
+  }
+  else if (service == "discord"){
+    const msg = new webhook.MessageBuilder()
+      .setName("NotifyBot")
+      .setText("<@!" + user + ">" + message);
+    try{
+      Hook.send(msg);
       res.send("1");
-  });
+    }
+    catch{
+      res.send("0");
+    }
+  }
 });
 
 app.listen(PORT, function () {
